21ee633fb04f6ca3ede58a1e3a21c89fc88e69a7
shunya-karan <karanyad9072@gmail.com>
2025-12-12 21:45:05 +0530

    deploy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,6 +23,14 @@ app.use(
   })
 );
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://busbuddy-hl0b.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 
 // Fix ES module dirname
 const __filename = fileURLToPath(import.meta.url);
